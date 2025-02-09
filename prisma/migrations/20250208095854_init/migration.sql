274e27c89ce575385f63dc7df1804b1d62180145
psolas <psolas@psoliou.com>
2025-02-09 10:07:50 +0200

    delete-migration

--- prisma/migrations/20250208095854_init/migration.sql
+++ prisma/migrations/20250208095854_init/migration.sql
@@ -1,15 +1,15 @@
 -- CreateTable
 CREATE TABLE "Product" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "price" REAL NOT NULL,
-    "orderId" INTEGER,
+    "orderId" TEXT,
     CONSTRAINT "Product_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Order" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "deliveryDate" DATETIME NOT NULL,
     "customerName" TEXT NOT NULL,
     "status" TEXT NOT NULL,
